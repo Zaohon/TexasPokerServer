67b0e9cc96350bd0e0d4da68fcecfef145f28056
Zaohon <1402767827@qq.com>
2023-07-18 10:52:46 +0800

    update

--- src/main/java/cn/blockmc/Zao_hon/TexasPokerServer.java
+++ src/main/java/cn/blockmc/Zao_hon/TexasPokerServer.java
@@ -14,52 +14,54 @@ import cn.blockmc.Zao_hon.command.CommandDispatcher;
 import cn.blockmc.Zao_hon.command.CommandHandler;
 import cn.blockmc.Zao_hon.command.CommandSender;
 
-
-public class TexasPokerServer implements CommandSender{
+public class TexasPokerServer implements CommandSender {
 	private static Logger logger = Application.logger;
 	private static TexasPokerServer instance = null;
+
 	public static TexasPokerServer get() {
-		if(instance == null) {
+		if (instance == null) {
 			instance = new TexasPokerServer();
 		}
 		return instance;
 	}
-	
+
 	public TexasPokerServer() {
 		this.setCommandExecutor(new CommandDispatcher(instance));
 		this.setChatExecutor(new ChatHandler());
 		new ServerConsoleThread().start();
 	}
-	
+
 	private CommandHandler commandExecutor = null;
 	private CommandHandler chatExecutor = null;
-	private HashMap<String,UserClient> clients = new HashMap<String,UserClient>();
-	
-	public void clientJoin(String name,UserClient client) {
-		logger.info(name+" has joined");
+	private HashMap<String, UserClient> clients = new HashMap<String, UserClient>();
+
+	public void clientJoin(String name, UserClient client) {
+		logger.info(name + " has joined");
 		clients.put(name, client);
 	}
+
 	public void clientQuit(String name) {
-		logger.info(name+" has quited");
+		logger.info(name + " has quited");
 		clients.remove(name);
 	}
-	
+
 	public UserClient getClient(String name) {
 		return clients.get(name);
 	}
-	
-	
-	public boolean commandExecute(CommandSender client,String cmd,String[] args) {
-		if(cmd.startsWith("/")) {
+
+	public boolean commandExecute(CommandSender client, String cmd, String[] args) {
+		if (cmd.startsWith("/")) {
 			cmd = cmd.substring(1);
 			return commandExecutor.handle(client, cmd, args);
-		}else {
+		} else {
 			return chatExecutor.handle(client, cmd, args);
 		}
 	}
+
 	public void setChatExecutor(CommandHandler handler) {
 		this.chatExecutor = handler;
 	}
+
 	public void setCommandExecutor(CommandHandler handler) {
 		this.commandExecutor = handler;
 	}
@@ -71,13 +73,12 @@ public class TexasPokerServer implements CommandSender{
 
 	@Override
 	public void sendMessages(List<String> list) {
-		list.forEach(str->sendMessage(str));
+		list.forEach(str -> sendMessage(str));
 	}
-	
+
 	public void broadcast(String str) {
-		clients.values().forEach(client->client.sendMessage(str));
+		clients.values().forEach(client -> client.sendMessage(str));
 		this.sendMessage(str);
 	}
-	
 
 }
